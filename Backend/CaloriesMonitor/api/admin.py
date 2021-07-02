f1b5363c6109eb67883e3b2df9d280d5be444849
Mayur Kanade <77790196+mayurkanade44@users.noreply.github.com>
2021-07-02 23:47:15 +0530

    Delete Backend/CaloriesMonitor/api directory

--- Backend/CaloriesMonitor/api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Data
-# Register your models here.
-
-# admin.site.register(Data)
-
-@admin.register(Data)
-class DataModel(admin.ModelAdmin):
-    list_display = ('date','meals','calories')
