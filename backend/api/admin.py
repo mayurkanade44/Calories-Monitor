d92122dab0eef74b6536ac42a8a31b3138de0fcc
mayurkanade44 <mayurkanade44@gmail.com>
2021-07-16 22:18:49 +0530

    backend added

---
+++ backend/api/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Data
+# Register your models here.
+
+# admin.site.register(Data)
+
+@admin.register(Data)
+class DataModel(admin.ModelAdmin):
+    list_display = ('date','meals','calories','user')
